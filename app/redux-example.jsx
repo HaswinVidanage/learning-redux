69a29d05bb402c691bec27ba0af497af35739a4d
Haswin Vidanage <Haswin Vidanage>
2017-04-30 17:23:15 +0530

    add action to add/remove hobbies and movies

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,7 +2,15 @@ var redux =  require('redux');
 
 console.log('starting redux example');
 
-var reducer = (state = {name : 'Anonymous'}, action) => {
+var stateDefault = {
+  name : 'Anonymous',
+  hobbies : [],
+  movies : []
+};
+var nextHobbyId = 1;
+var nextMovieId = 1;
+
+var reducer = (state = stateDefault, action) => {
   //state = state || {name : 'Anonymous'} //es5
 
   switch (action.type) {
@@ -11,6 +19,40 @@ var reducer = (state = {name : 'Anonymous'}, action) => {
         ...state,
         name: action.name
       };
+    case 'ADD_HOBBY':
+      return {
+        ...state,
+        hobbies: [
+          ...state.hobbies,
+          {
+            id : nextHobbyId++,
+            hobby :action.hobby,
+            genre :action.genre
+          }
+        ]
+      };
+    case 'ADD_MOVIE':
+      return {
+        ...state,
+        movies : [
+          ...state.movies,
+          {
+            id : nextMovieId++,
+            movie_name : action.name,
+
+          }
+        ]
+      };
+    case 'REMOVE_HOBBY':
+      return {
+        ...state,
+        hobbies : state.hobbies.filter((hobby) => hobby.id !== action.id)
+      };
+    case 'REMOVE_MOVIE':
+      return {
+        ...state,
+        movies : state.movies.filter((movie) => movie.id !== action.id)
+      }
     default:
       return state;
   }
@@ -26,7 +68,8 @@ var unsubscribe = store.subscribe(() => {
   var state = store.getState();
 
   console.log('Name is ', state.name);
-  document.getElementById('app').innerHTML = state.name; 
+  document.getElementById('app').innerHTML = state.name;
+  console.log('New State ' , store.getState() );
 
 });
 
@@ -42,6 +85,51 @@ store.dispatch({
 // unsubscribe(); // store.subscribe RETURNS  a method which when called will unsubscribe.
 
 store.dispatch({
+  type : 'ADD_HOBBY',
+  hobby : 'Running'
+});
+
+store.dispatch({
+  type : 'ADD_HOBBY',
+  hobby : 'Swim'
+});
+
+store.dispatch({
+  type : 'ADD_HOBBY',
+  hobby : 'Walking'
+});
+
+store.dispatch({
+  type : 'REMOVE_HOBBY',
+  id : 2
+});
+
+
+store.dispatch({
+  type : 'ADD_MOVIE',
+  name : 'Star Wars',
+  genre : 'sci-fi'
+});
+
+store.dispatch({
   type : 'CHANGE_NAME',
   name : 'Andrew'
+});
+
+store.dispatch({
+  type : 'ADD_MOVIE',
+  name : 'South Park',
+  genre : 'comedy'
+});
+
+store.dispatch({
+  type : 'ADD_MOVIE',
+  name : 'Star Wars 3',
+  genre : 'sci-fi'
+});
+
+
+store.dispatch({
+  type : 'REMOVE_MOVIE',
+  id : 2
 });
