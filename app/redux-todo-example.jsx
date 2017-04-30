576e040730099fd2e716d19fae0c33a07c77fb75
Haswin Vidanage <Haswin Vidanage>
2017-04-30 12:37:47 +0530

    add support for change name action

--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -8,10 +8,26 @@ var stateDefault = {
   todos:[]
 };
 var reducer = (state = stateDefault, action) => {
-  return state;
+  switch (action.type) {
+    case 'CHANGE_SEARCH_TEXT':
+      return {
+        ...state,
+        searchText:action.searchText
+      };
+    default:
+      return state;
+  }
 };
 
 var store = redux.createStore(reducer);
 
 var currentState = store.getState();
 console.log('currentState', currentState);
+
+
+store.dispatch({
+  type : 'CHANGE_SEARCH_TEXT',
+  searchText : 'Haswin Search'
+});
+
+console.log('After New Action', store.getState());
