4fbba5678251f9a6476ac8bb76b2793e6fe49bef
Haswin Vidanage <Haswin Vidanage>
2017-05-01 13:11:39 +0530

    add support for async location fetching

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,5 +1,5 @@
 var redux =  require('redux');
-
+var axios = require('axios');
 console.log('starting redux example');
 
 //Name reducer and action generators
@@ -92,11 +92,57 @@ var removeMovie = (id) => {
   }
 };
 
+//Map reducer and action generators
+//---
+var mapReducer = (state = {isFetching: false, url: undefined }, action) => {
+  switch (action.type) {
+    case 'START_LOCATION_FETCH':
+        return {
+          isFetching: true,
+          url: undefined
+        };
+    case 'COMPLETE_LOCATION_FETCH':
+        return {
+          isFetching: false,
+          url: action.url
+        };
+    default:
+        return state;
+
+  }
+};
+
+var startLocationFetch = () => {
+  return {
+    type : 'START_LOCATION_FETCH'
+  }
+};
+
+var completeLocationFetch = (url) => {
+    return {
+      type : 'COMPLETE_LOCATION_FETCH',
+      url
+    }
+};
+
+var fetchLocation = () => {
+    store.dispatch(startLocationFetch());
+
+    axios.get('http://ipinfo.io').then(function(res){
+      var loc = res.data.loc;
+      var baseUrl = 'http://maps.google.com?q=';
+      store.dispatch(completeLocationFetch(baseUrl + loc));
+    });
+};
+
 var reducer = redux.combineReducers({
   name : nameReducer,
   hobbies : hobbiesReducer,
-  movies : moviesReducer
+  movies : moviesReducer,
+  map : mapReducer
 });
+
+
 
 var store = redux.createStore(reducer, redux.compose(
   window.devToolsExtension ? window.devToolsExtension() : f => f
@@ -107,7 +153,14 @@ var unsubscribe = store.subscribe(() => {
   var state = store.getState();
 
   console.log('Name is ', state.name);
-  document.getElementById('app').innerHTML = state.name;
+
+
+  if(state.map.isFetching){
+    document.getElementById('app').innerHTML = 'Loading...';
+  } else if (state.map.url){
+    document.getElementById('app').innerHTML = '<a target="_blank" href ="'+state.map.url+ '">View your location</a>';
+  }
+
   console.log('New State ' , store.getState() );
 
 });
@@ -115,6 +168,7 @@ var unsubscribe = store.subscribe(() => {
 var currentState = store.getState();
 console.log('currentState ' , currentState );
 
+fetchLocation();
 
 // store.dispatch({
 //   type : 'CHANGE_NAME',
